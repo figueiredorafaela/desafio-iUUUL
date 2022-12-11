c3ce0344f7524448a13d3ef3bf8b9583798b17d3
Rafaela <rafaelaffigueiredo1@gmail.com>
2022-12-11 20:34:31 -0300

    botao deleta

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
 const form = document.getElementById("newMessage")
+const deleta = document.getElementById("deleta")
 const contatos = JSON.parse(localStorage.getItem("contatos")) || []
 
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-    
+
     const nome = evento.target.elements['nome']
     const email = evento.target.elements['email']
     const mensagem = evento.target.elements['mensagem']
-    const deleta = document.getElementById("deleta")
     
     guardaDados(nome.value, email.value, mensagem.value)
-    
+    pegaDados(contatos)
+
+    console.log("submit")
+
     nome.value=""
     email.value=""
     mensagem.value=""
 
-    pegaDados(contatos)
- 
 })
 
-deleta.addEventListener("click", function(){
-    const resultado = window.confirm("Você tem certeza de que deseja DELETAR todas as mensagens?")
-    if(resultado == true)
-        localStorage.clear()
+deleta.addEventListener("click", (evento) => {
+    const confirma = window.confirm("Você tem certeza de que deseja DELETAR todas as mensagens?")
+    if(confirma == true){
+        contatos.length = 0
+        console.log("apagando")
+        console.log(contatos)
+    }
+        
 })
 
 
@@ -36,16 +41,18 @@ function guardaDados(nome, email, mensagem) {
     }
 
     contatos.push(mensagemAtual)
-
+    console.log("guardando")
     localStorage.setItem("contatos", JSON.stringify(contatos))
-    
+
 }
 
 function pegaDados(contatos){
 
     for (let i = 0; i < contatos.length; i++) {
         const dados = contatos[i];
+        console.log("dados:")
         console.log(dados)
 
    }
 }
+
