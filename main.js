407fcb7b31e3717d65a6ce3cf59c1878ad3fe053
Rafaela <rafaelaffigueiredo1@gmail.com>
2022-12-12 13:47:52 -0300

    desafio 2 completo

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const form = document.getElementById("newMessage")
+const form = document.getElementById("novaMensagem")
 const deleta = document.getElementById("deleta")
 const contatos = JSON.parse(localStorage.getItem("contatos")) || []
 
@@ -11,28 +11,25 @@ form.addEventListener("submit", (evento) => {
     const mensagem = evento.target.elements['mensagem']
     
     guardaDados(nome.value, email.value, mensagem.value)
-    pegaDados(contatos)
-
-    console.log("submit")
 
     nome.value=""
     email.value=""
     mensagem.value=""
 
+    window.location = 'mensagens.html'
 })
 
 deleta.addEventListener("click", (evento) => {
     const confirma = window.confirm("Você tem certeza de que deseja DELETAR todas as mensagens?")
+
     if(confirma == true){
+        localStorage.clear()
         contatos.length = 0
-        console.log("apagando")
-        console.log(contatos)
     }
         
 })
 
-
-function guardaDados(nome, email, mensagem) {
+function guardaDados(nome, email, mensagem)  {
 
     const mensagemAtual ={
         "nome": nome,
@@ -41,18 +38,7 @@ function guardaDados(nome, email, mensagem) {
     }
 
     contatos.push(mensagemAtual)
-    console.log("guardando")
+ 
     localStorage.setItem("contatos", JSON.stringify(contatos))
-
-}
-
-function pegaDados(contatos){
-
-    for (let i = 0; i < contatos.length; i++) {
-        const dados = contatos[i];
-        console.log("dados:")
-        console.log(dados)
-
-   }
 }
 
